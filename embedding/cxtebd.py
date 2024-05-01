a994c39b3d0817a13a5ff02f2a83de98c1f018fe
YS19999 <117980405+YS19999@users.noreply.github.com>
2024-05-01 15:28:21 +0800

    Update cxtebd.py

--- embedding/cxtebd.py
+++ embedding/cxtebd.py
@@ -26,15 +26,7 @@ class CXTEBD(nn.Module):
 
         print("{}, Loading pretrainedModel {}".format(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), args.pretrained_model), flush=True)
 
-        # self.model = BertModel.from_pretrained("../module/bert-base-uncased")
         self.model = AutoModel.from_pretrained(args.pretrained_model, ignore_mismatched_sizes=True)
-        # self.unfreeze_layers = ['layer.11']
-        # self.unfreeze_layers = ['layer.11', 'pooler.']
-        # for name, param in self.model.named_parameters():
-        #     param.requires_grad = False
-        #     for ele in self.unfreeze_layers:
-        #         if ele in name:
-        #             param.requires_grad = True
 
         self.embedding_dim = self.model.config.hidden_size
         self.ebd_dim = self.model.config.hidden_size
@@ -51,24 +43,6 @@ class CXTEBD(nn.Module):
         # need to use smaller batches
         out = self.model(input_ids=bert_id, attention_mask=mask)
 
-        # return seq of bert ebd, dim: batch, text_len, ebd_dim
-        # return last_layer[:,1:-1,:]
-
-        if self.args.meta_w_target:
-            # count length excluding <pad> and <unk>.
-            is_zero = (torch.sum(torch.abs(out[0]), dim=2) > 1e-8).float()
-            soft_len = torch.sum(is_zero, dim=1, keepdim=True)
-
-            soft_len[soft_len < 1] = 1
-            # # don't need to mask out the <pad> tokens, as the embeddings are zero
-            ebd = torch.sum(out[0], dim=1)
-
-            ebd = ebd / soft_len
-
-            w = stats.get_w_target_rr(data, ebd, self.args.meta_w_target_lam)
-
-            return out[0], w.detach()
-
         if self.return_seq:
             return out[0]
         else:
@@ -78,8 +52,6 @@ class CXTEBD(nn.Module):
 
         with torch.no_grad():
             x = self.get_bert(data['text'], data['attn_mask'], data)
-            if (self.args.classifier == "mn" or self.args.classifier == "contrastnet") and self.args.embedding == "ebd":
-                x_aug = self.get_bert(data['aug_text'], data['aug_mask'], data)
-                return x, x_aug
-            return x
+            x_aug = self.get_bert(data['aug_text'], data['aug_mask'], data)
+            return x, x_aug
 
