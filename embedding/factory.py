e776a5ed047f91d9b5eb6d3e05d3588cf0f77c63
YS19999 <117980405+YS19999@users.noreply.github.com>
2024-05-01 15:27:29 +0800

    Update factory.py

--- embedding/factory.py
+++ embedding/factory.py
@@ -1,35 +1,16 @@
 import torch
 import datetime
 
-from embedding.wordebd import WORDEBD
 from embedding.cxtebd import CXTEBD
-
 from embedding.avg import AVG
-from embedding.cnn import CNN
-from embedding.idf import IDF
-from embedding.meta import META
-from embedding.lstmatt import LSTMAtt
-
 
 def get_embedding(vocab, args):
     print("{}, Building embedding: {}".format(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S'), args.embedding), flush=True)
 
     # check if loading pre-trained embeddings
-    # ebd = WORDEBD(vocab, args)
     ebd = CXTEBD(args, return_seq=(args.embedding != 'ebd'))
 
-    if args.embedding in ['meta', 'meta_mlp']:
-        model = META(ebd, args)
-    elif args.embedding == 'avg' and args.bert:
-        model = AVG(ebd, args)  # using bert representation directly
-    elif args.embedding == 'ebd' and args.bert:
-        model = ebd  # using bert representation directly
-    elif args.embedding == 'cnn' and args.bert:
-        model = CNN(ebd, args)  # using bert representation directly
-    elif args.embedding == 'lstmatt' and args.bert:
-        model = LSTMAtt(ebd, args)  # using bert representation directly
-    else:
-        model = None
+    model = AVG(ebd, args)  # using bert representation directly
 
     print("{}, Building embedding".format(datetime.datetime.now().strftime('%y/%m/%d %H:%M:%S')), flush=True)
 
